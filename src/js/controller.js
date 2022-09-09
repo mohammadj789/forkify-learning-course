bb322691c4e544e6c79b1340641a21b397b8b936
mohammadj789 <mohammadsoltanian10@gmail.com>
2022-09-10 02:54:03 +0430

    add new wellcome message

--- src/js/controller.js
+++ src/js/controller.js
@@ -117,6 +117,9 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(error.message);
   }
 };
+const newFeature = function () {
+  console.log('welcome to the application');
+};
 
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmark);
@@ -126,5 +129,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
